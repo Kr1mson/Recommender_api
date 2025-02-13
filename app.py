7b8196815a63f7f994bc258c7666ee61197f4ad8
Parth Passi <120441768+Kr1mson@users.noreply.github.com>
2025-02-13 21:08:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
+from flask_cors import CORS
 
 print("Loading data and model...")
 df = pd.read_csv('data.csv')
@@ -11,7 +12,7 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 loaded_index = faiss.read_index('index_file.index')
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/recommend', methods=['POST'])
 def recommend_games():
     user_query = request.json.get("query")
@@ -25,9 +26,11 @@ def recommend_games():
 
     distances, indices = loaded_index.search(query_embedding, top_n)
 
-    recommendations = df.iloc[indices[0]]["name"].tolist()
+    recommendations = df.iloc[indices[0]]["id"].tolist()
     
     return jsonify({"recommendations": recommendations})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
